a1d79bd03fb930862e4cd1fb9275403451bc94c4
mohamed_eid <eng.meid404@gmail.com>
2024-11-27 04:29:07 +0200

    done

--- terraform/main.tf
+++ terraform/main.tf
@@ -45,66 +45,66 @@ resource "aws_security_group" "agent_sg" {
   }
 }
 
-# resource "aws_security_group" "prometheus_sg" {
-#   name        = "prometheus_sg"
-#   description = "Allow inbound traffic for Prometheus Server"
+resource "aws_security_group" "prometheus_sg" {
+  name        = "prometheus_sg"
+  description = "Allow inbound traffic for Prometheus Server"
 
-#   ingress {
-#     from_port   = 80
-#     to_port     = 80
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-#   ingress {
-#     from_port   = 22
-#     to_port     = 22
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-#   ingress {
-#     from_port   = 9100
-#     to_port     = 9100
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
+  ingress {
+    from_port   = 9100
+    to_port     = 9100
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-#   ingress {
-#     from_port   = 8080
-#     to_port     = 8080
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-#   ingress {
-#     from_port   = 9090
-#     to_port     = 9090
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
+  ingress {
+    from_port   = 9090
+    to_port     = 9090
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-#   ingress {
-#     from_port   = 9093
-#     to_port     = 9093
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
+  ingress {
+    from_port   = 9093
+    to_port     = 9093
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-#   ingress {
-#     from_port   = 3000
-#     to_port     = 3000
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-# }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
 resource "aws_instance" "agent" {
   ami             = "ami-0e86e20dae9224db8" # Ubuntu AMI (modify for your region)
@@ -117,16 +117,16 @@ resource "aws_instance" "agent" {
   }
 }
 
-# resource "aws_instance" "prometheus" {
-#   ami             = "ami-0e86e20dae9224db8"
-#   instance_type   = var.instance_type
-#   key_name        = var.ssh_key_name
-#   security_groups = [aws_security_group.prometheus_sg.name]
+resource "aws_instance" "prometheus" {
+  ami             = "ami-0e86e20dae9224db8"
+  instance_type   = var.instance_type
+  key_name        = var.ssh_key_name
+  security_groups = [aws_security_group.prometheus_sg.name]
 
-#   tags = {
-#     Name = "prometheus_server"
-#   }
-# }
+  tags = {
+    Name = "prometheus_server"
+  }
+}
 
 # Outputs for the agent instance
 output "agent_public_ip" {
@@ -137,8 +137,8 @@ output "agent_private_ip" { # Rename this to agent_private_ip for consistency
   value = aws_instance.agent.private_ip
 }
 
-# # Outputs for the Prometheus server
-# output "prometheus_public_ip" {
-#   value = aws_instance.prometheus.public_ip
-# }
+# Outputs for the Prometheus server
+output "prometheus_public_ip" {
+  value = aws_instance.prometheus.public_ip
+}
 
