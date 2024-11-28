9ba9d3991d75fc6d3c5ebc2290656f46dd4315be
mohamed_eid <eng.meid404@gmail.com>
2024-11-28 06:51:53 +0200

    test12

--- src/eslint.config.js
+++ src/eslint.config.js
@@ -1,13 +1,16 @@
-module.exports = [
-    {
-      files: ["**/*.js"],
-      languageOptions: {
-        ecmaVersion: 2021,
-        sourceType: "module",
-      },
-      rules: {
-        "no-console": "off", // Allows console statements
-      },
+import globals from 'globals';
+
+/** @type {import('eslint').Linter.FlatConfig[]} */
+export default [
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
+      globals: globals.node,
     },
-  ];
-  
+    rules: {
+      'no-console': 'off',
+    },
+  },
+];
